bc8d70f75d61668f2e96b0a7852f054d383e8b76
Gabriel <jgorosco@aol.com>
2023-08-15 23:16:33 -0500

    weather-app finished

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# weather-app
+
+# Weather App
+
+Una aplicación para buscar el clima dada una ciudad.
+
+
+
+## Screenshots
+
+![App Screenshot](./screenshots/screenshot-1.png)
+
+## Run locally
+
+Para desplegar basta con utilizar un live server
+
+- Añadir un archivo keys.js y dentro de este agregar una constante llamada "apiKey" y exportándola, colocando la llave de la api para obtener el clima que se puede obtener en https://openweathermap.org/
+
---
+++ weather.js
@@ -0,0 +1,41 @@
+import apiKey from './keys.js';
+
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=es&q="
+
+const weathers = {
+    CLOUDS: "Clouds",
+    CLEAR: "Clear",
+    RAIN: "Rain",
+    DRIZZLE: "Drizzle",
+    MIST: "Mist"
+}
+
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city.toLowerCase() + `&appid=${apiKey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = city;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        weatherIcon.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+        weatherIcon.title = data.weather[0].description
+
+        document.querySelector(".weather").style.display = "block";
+    }
+}
+
+searchButton.addEventListener("click", () => {
+    checkWeather(searchBox.value)
+})
